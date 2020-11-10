62e121046e771cbd6517aa3e1a7d9298f065663c
Colleen King <colleenking0810@gmail.com>
2020-11-10 14:02:20 -0700

    added image to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,55 @@
 <template>
-<b-container class="home">
 
-  <b-container class="heading">
-    <b-row>
-      <b-col>The Collection of Blathers' Blatherings</b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        Sleeping by day, Blathering by night
-      </b-col>
-    </b-row>
+  <b-container class="home">
+
+    <b-container class="heading">
+      <b-row class="mb-2">
+        <b-col>
+          The Collection of Blathers' Blatherings
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          Sleeping by day, Blathering by night
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-container>
+      <b-row>
+        <b-col>
+          <img src="../assets/acnh-muesum.png" class="img-fluid" style="max-width:800px">
+        </b-col>
+      </b-row>
+    </b-container>
 
   </b-container>
 
-</b-container>
 </template>
 
 <script>
-// @ is an alias to /src
 
-export default {
-  name: 'Home',
-  // components: {
-  //   HelloWorld
-  // }
-}
+  // @ is an alias to /src
+
+  export default {
+    name: 'Home',
+    components: {}
+  }
+
 </script>
 
 <style>
-.home {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 
-.heading {
-  color: black;
-  font-size: 20px;
-}
+  .home {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .heading {
+    font-size: 25px;
+    font-weight: bold;
+  }
+
 </style>
